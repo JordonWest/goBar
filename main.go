21948fbc72a65d89dddcc01a4f0e0d67c61f3587
Jordan West <jwest@drwholdings.com>
2020-12-16 13:45:10 -0600

    adding structures for beer

--- main.go
+++ main.go
@@ -3,10 +3,30 @@ package main
 
 import (
   "fmt"
-  "github.com/goBar/data"
+//  "github.com/goBar/data"
 )
 
-func main() {
-  fmt.Println("Hello world!")
-  data.Init()
+type Beer struct{
+  name, brand string
+  abv, rating int
 }
+
+func (beer Beer) beer_stats() {
+  return Beer
+
+func show_inv(list []string) {
+  for _, beer := range list {
+    fmt.Printf("Beer name: %v\n", beer)
+  }
+}
+func welcome() string{
+  return "welcome to the beer store!"
+}
+
+func main() {
+  inventory := []string{"beer 1", "beer 2", "beer 3"}
+  fmt.Println("Hello world!")
+  show_inv(inventory)
+  fmt.Println(welcome())
+}
+
