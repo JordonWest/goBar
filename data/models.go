f33a73c593ee9fd303e095dd14cb23df59d952f6
jordywestside <mm1jwest@gmail.com>
2020-12-17 20:00:07 -0600

    removed unused legacy from models.go

--- data/models.go
+++ data/models.go
@@ -8,7 +8,6 @@ import (
 
 func Init() {
   db, _ :=sql.Open("sqlite3", "./booze.db")
-  //checkErr(err)
 
   statement, _ := db.Prepare("CREATE TABEL IF NOT EXISTS beers (id INTEGER PRIMARY KEY, name TEXT, stock INTEGER)")
   statement.Exec()
